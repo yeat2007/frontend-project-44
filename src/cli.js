fe896786bd00a8acf59c4ada71c7906721ca5f9e
yeat2007 <7558414@list.ru>
2025-02-25 14:14:47 +0300

    fix

--- src/cli.js
+++ src/cli.js
@@ -2,9 +2,13 @@ import readlineSync from 'readline-sync';
 
 
 export default function hello(){
+    
+    
+
     console.log('Welcome to the Brain Games!')
 
-    const name = readlineSync.question('May I have your name? ');
+
+    const name = readlineSync.question('May I have your name? ')
 
     console.log('Hello, ' + name + '!')
     return name
