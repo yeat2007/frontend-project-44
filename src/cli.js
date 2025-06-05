560df50225907f6af1e46622b1327347271cd6fe
arseniy <164155645+yeat2007@users.noreply.github.com>
2025-06-05 14:24:37 +0300

    Update cli.js

--- src/cli.js
+++ src/cli.js
@@ -2,10 +2,10 @@ import readlineSync from 'readline-sync';
 
 
 export default function hello(){
-    console.log('Welcome to the Brain Games!')
-    const name = readlineSync.question('May I have your name?')
-    console.log(`Hello, ${name} !`)
-    return name
+    console.log('Welcome to the Brain Games!');
+    const name = readlineSync.question('May I have your name?');
+    console.log(`Hello, ${name} !`);
+    return name;
 }
 
 
