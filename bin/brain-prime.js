b2aeb5873a5059b987b9b671201a38a6a6e253f3
arseniy <164155645+yeat2007@users.noreply.github.com>
2025-06-05 14:23:46 +0300

    Update brain-prime.js

--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
 import gameRounds from '../src/index.js';
-import { getQuestAndAnswer, rule } from '../src/games/prime.js'
+import { getQuestAndAnswer, rule } from '../src/games/prime.js';
 
 gameRounds(getQuestAndAnswer , rule);
