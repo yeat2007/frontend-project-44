79f5074cd9b2d93f6f926821002c30c35eeba38a
arseniy <164155645+yeat2007@users.noreply.github.com>
2025-06-05 13:22:59 +0300

    Update prime.js

--- src/games/prime.js
+++ src/games/prime.js
@@ -3,12 +3,12 @@ import randomNum from '../utils.js';
 
 const isPrime =(num) =>{
     if (num < 2){
-        return "no";
+        return "false";
     } 
     for (let i =2; i < num / 2; i += 1){
-        if (num % i ===0) return "no";
+        if (num % i ===0) return "false";
     }
-    return "yes";
+    return "true";
 }
 
 
