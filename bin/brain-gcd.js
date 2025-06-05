647f2031cdfd57b24025e7b3679e202afbc15085
arseniy <164155645+yeat2007@users.noreply.github.com>
2025-06-05 14:38:14 +0300

    Update brain-gcd.js

--- bin/brain-gcd.js
+++ bin/brain-gcd.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-import gameRounds from '../src/index.js';
-import { getQuestAndAnswer, rule } from '../src/games/gcd.js';
 
-gameRounds(getQuestAndAnswer , rule);
+import gameRounds from '../src/index.js'
+import { getQuestAndAnswer, rule } from '../src/games/gcd.js'
+gameRounds(getQuestAndAnswer, rule)
