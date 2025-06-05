f292977754b6d77f8864b636025275599cbf2d68
arseniy <164155645+yeat2007@users.noreply.github.com>
2025-06-05 14:36:06 +0300

    Update brain-calc.js

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -2,5 +2,5 @@
 
 import gameRounds from '../src/index.js'
 import { getQuestAndAnswer, rule } from '../src/games/calc.js'
-gameRounds(getQuestAndAnswer , rule);
+gameRounds(getQuestAndAnswer, rule)
 
