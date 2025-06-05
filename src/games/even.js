8962f26f134d6158e7b33d159e5aee86b87335df
arseniy <164155645+yeat2007@users.noreply.github.com>
2025-06-05 14:43:09 +0300

    Update even.js

--- src/games/even.js
+++ src/games/even.js
@@ -1,9 +1,8 @@
-import randomNum from '../utils.js';
-
-const gameEven = (num) => num % 2 === 0     
+import randomNum from '../utils.js'
+const gameEven = num => num % 2 === 0
 export const rule = 'Answer "yes" if the number is even, otherwise answer "no".'
-export function getQuestAndAnswer () {
-    const quest = randomNum();
-    const answer = gameEven(quest) ? 'yes' : 'no';
+export function getQuestAndAnswer() {
+    const quest = randomNum()
+    const answer = gameEven(quest) ? 'yes' : 'no'
     return [quest, answer]
 }
