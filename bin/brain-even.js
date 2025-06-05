ed00b7384ce9adc2a736891a03c5bab2c5b20321
arseniy <164155645+yeat2007@users.noreply.github.com>
2025-06-05 13:09:14 +0300

    Update brain-even.js

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-import { gameRounds } from '../src/index.js';
+import gameRounds from '../src/index.js';
 import { getQuestAndAnswer, rule } from '../src/games/even.js';
 
 gameRounds( getQuestAndAnswer , rule);
