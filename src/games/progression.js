ce5b9b1db65df80f013cc66cc97dd4b77f9361e7
arseniy <164155645+yeat2007@users.noreply.github.com>
2025-06-05 13:36:26 +0300

    Update progression.js

--- src/games/progression.js
+++ src/games/progression.js
@@ -1,29 +1,26 @@
 import randomNum from '../utils.js';
 
-
-
 export const rule = 'What number is missing in the progression?';
+
 export function getQuestAndAnswer() {
-    const start = randomNum()
-    const process = randomNum()
-    const quest= [start];
+  const length = 10;
+  const first = randomNum(1, 10); // начальное значение
+  const step = randomNum(2, 5);   // шаг прогрессии
+  const hiddenIndex = randomNum(0, length - 1); // индекс скрытого элемента
 
-    const progressionLength = 9
-    for (let i = 1; i<= progressionLength; i+=1){
-        quest[i] = quest[i-1] + process
+  const progression = [];
 
-
-    }
-    const index = randomNum()
-    const answer = String(quest[index])
-    const progressionGap = [...quest];
-    progressionGap[index] = '..'
-    return [progressionGap.join(' '), answer];
+  for (let i = 0; i < length; i++) {
+    progression.push(first + i * step);
   }
 
+  const answer = String(progression[hiddenIndex]);
+  progression[hiddenIndex] = '..';
 
+  const question = progression.join(' ');
 
-
+  return [question, answer];
+}
 
 
 
