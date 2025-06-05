1fed07ac96257897ca85740a3ffbf4b956a2272e
arseniy <164155645+yeat2007@users.noreply.github.com>
2025-06-05 14:26:18 +0300

    Update calc.js

--- src/games/calc.js
+++ src/games/calc.js
@@ -4,23 +4,23 @@ import randomNum from '../utils.js';
 const getCount = (randomNum, operator, randomNum2) => {
     switch(operator){
         case '+':
-            return randomNum + randomNum2
+            return randomNum + randomNum2;
         case '*':
-            return randomNum * randomNum2
+            return randomNum * randomNum2;
         case '-':
-            return randomNum - randomNum2
+            return randomNum - randomNum2;
         default: return 'Error'
     }
 }
 
 export const rule = 'What is the result of the expression?';
 export const getQuestAndAnswer = () => {
-    const point = ['+', '-', '*']
-    const randomPoint = Math.floor(Math.random() * 3)
-    const randomOperator = point[randomPoint]
+    const point = ['+', '-', '*'];
+    const randomPoint = Math.floor(Math.random() * 3);
+    const randomOperator = point[randomPoint];
 
-  const num = randomNum()
-  const num2 = randomNum()
+  const num = randomNum();
+  const num2 = randomNum();
 
   const quest = `${num} ${randomOperator} ${num2}`
   const answer = String(getCount(num, randomOperator, num2))
