2a993ceb6fce50f364f2590fe3c48f8801871a0b
arseniy <164155645+yeat2007@users.noreply.github.com>
2025-06-05 13:36:44 +0300

    Update utils.js

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,4 @@
-const randomNum = (min = 2, max = 100) => {
+const randomNum = (min = 1, max = 100) => {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }; 
 export default randomNum;
