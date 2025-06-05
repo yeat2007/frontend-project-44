527cb79eb2be93047325daf439e6fd3858905c1a
arseniy <164155645+yeat2007@users.noreply.github.com>
2025-06-05 13:42:01 +0300

    Update prime.js

--- src/games/prime.js
+++ src/games/prime.js
@@ -1,17 +1,15 @@
 import randomNum from '../utils.js';
 
 
-const isPrime =(num) =>{
-    if (num < 2){
-        return "false";
-    } 
-    for (let i =2; i < num / 2; i += 1){
-        if (num % i ===0) return "false";
-    }
-    return "true";
-}
 
 
+const isPrime = (num) => {
+  if (num < 2) return false;
+  for (let i = 2; i <= Math.sqrt(num); i++) {
+    if (num % i === 0) return false;
+  }
+  return true;
+};
 
 export const rule = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 export function getQuestAndAnswer() {
