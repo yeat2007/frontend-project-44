6e8e77a0ef2ff875aa5ed217c94c9d648dc417a3
arseniy <164155645+yeat2007@users.noreply.github.com>
2025-06-05 15:38:40 +0300

    Update cli.js

--- src/cli.js
+++ src/cli.js
@@ -3,5 +3,5 @@ export default function hello() {
   console.log('Welcome to the Brain Games!')
   const name = readlineSync.question('May I have your name?')
   console.log(`Hello, ${name} !`)
-return name
+  return name
 }
