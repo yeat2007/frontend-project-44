81aa14e591208e11dff847f003c07c0d0f844d77
arseniy <164155645+yeat2007@users.noreply.github.com>
2025-06-05 15:33:44 +0300

    Update gcd.js

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -1,12 +1,12 @@
 import randomNum from '../utils.js'
 const gcd = (num, num2) => {
-    if (num < num2){
-        return gcd(num2, num)
-    }
-    if (!num2) {
-        return String(num)
-    }
-    return gcd(num2, num % num2)
+  if (num < num2) {
+  return gcd(num2, num)
+  }
+  if (!num2) {
+  return String(num)
+  }
+  return gcd(num2, num % num2)
 }
 export const rule = 'Find the greatest common divisor of given numbers.'
 export function getQuestAndAnswer() {
