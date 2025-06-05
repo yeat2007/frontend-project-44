c5bad936cfda46a211148dcd1d9ab201b6c83175
arseniy <164155645+yeat2007@users.noreply.github.com>
2025-06-05 14:40:13 +0300

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,7 @@
 import globals from 'globals';
 import pluginJs from '@eslint/js';
-
-
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {languageOptions: { globals: globals.browser }},
+  { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
 ];
-
-
