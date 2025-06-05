7bba5df86dbbd9ca18b2140e492c500e7b2cbb91
arseniy <164155645+yeat2007@users.noreply.github.com>
2025-06-05 14:24:01 +0300

    Update brain-progression.js

--- bin/brain-progression.js
+++ bin/brain-progression.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
 import gameRounds from '../src/index.js';
-import { getQuestAndAnswer, rule } from '../src/games/progression.js'
+import { getQuestAndAnswer, rule } from '../src/games/progression.js';
 
 gameRounds(getQuestAndAnswer , rule);
